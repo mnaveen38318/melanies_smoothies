023b9f079983d9b91ba4b18c5167b7a151a5c560
mnaveen38318 <165916328+mnaveen38318@users.noreply.github.com>
2024-04-03 20:31:37 +0530

    Update streamlit_app.py
    space

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ if ingrediets_list:
     ingrediemts_str = ''
     for f_choosen in ingrediets_list:
         ingrediemts_str+=f_choosen +' '
-        st.subheader(f_choosen+'Nutritin Information')
+        st.subheader(f_choosen+' Nutritin Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         
